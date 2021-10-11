b7638c5b79f5f16bec75c5f3587fb662448a0728
Joohyuck <joohyuck.seo@gmail.com>
2021-10-12 02:48:02 +0900

    오른손 법칙

--- Maze/Player.h
+++ Maze/Player.h
@@ -1,9 +1,14 @@
-#pragma once
+﻿#pragma once
 
 class Board;
 
 class Player
 {
+	enum
+	{
+		MOVE_TICK = 100,
+	};
+
 public:
 	void	Init(Board* board);
 	void	Update(uint64 deltaTick);
@@ -11,9 +16,15 @@ public:
 	void	SetPos(Pos pos) { _pos = pos; }
 	Pos		GetPos() { return _pos; }
 
+	bool	CanGo(Pos pos);
+
 private:
 	Pos		_pos = {};
 	int32	_dir = DIR_UP;
 	Board*	_board = nullptr;
+
+	vector<Pos> _path;
+	uint32		_pathIndex = 0;
+	uint64		_sumTick = 0;
 };
 
