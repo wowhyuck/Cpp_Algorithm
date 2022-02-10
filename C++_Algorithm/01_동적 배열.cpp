1a5d1caa27f7e30020c2a12b114ac424efc39be9
Joohyuck <joohyuck.seo@gmail.com>
2022-02-11 01:21:32 +0900

    동적 배열 구현 연습

---
+++ C++_Algorithm/01_동적 배열.cpp
@@ -0,0 +1,110 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+template<typename T>
+class Vector
+{
+public:
+	Vector()
+	{
+
+	}
+
+	~Vector()
+	{
+		if (_data)
+			delete[] _data;
+
+
+	}
+
+	void push_back(const T& value)
+	{
+		if (_size == _capacity)
+		{
+			// 증설 작업
+			int newCapacity = static_cast<int>(_capacity * 1.5);
+			if (newCapacity == _capacity)
+				newCapacity++;
+
+			reserve(newCapacity);
+		}
+
+		// 데이터 저장
+		_data[_size] = value;
+
+		// 데이터 개수 증가
+		_size++;
+
+	}
+
+	void reserve(int capacity)
+	{
+		if (_capacity >= capacity)
+			return;
+
+		_capacity = capacity;
+
+		T* newData = new T[_capacity];
+
+		// 데이터 복사
+		for (int i = 0; i < _size; i++)
+			newData[i] = data[i];
+
+		if (_data)
+			delete[] _data;
+
+		// 교체
+		_data = newData;
+	}
+
+	T& operator[](const int pos) { return _data[pos]; }
+
+	int size() { return _size; }
+	int capacity() { return _capacity; }
+
+	void clear()
+	{
+		if (_data)
+		{
+			delete[] _data;
+			_data = new T[_capacity];
+		}
+
+
+		_size = 0;
+	}
+
+
+private:
+	T*	_data = nullptr;
+	int _size = 0;
+	int _capacity = 0;
+};
+
+
+int main()
+{
+	vector<int> v;
+	//Vector<int> v;
+
+	// resize -> size를 조정
+	v.resize(10);
+
+	// reserve -> capacity를 조정
+	v.reserve(100);		// 처음에 개수를 여유있게 잡으면 메모리 이동이 사라져 효율이 높아진다.
+	cout << v.size() << " " << v.capacity() << endl;
+
+	for (int i = 0; i < 100; i++)
+	{
+		v.push_back(i);
+		cout << v[i] << " " << v.size() << " " << v.capacity() << endl;
+	}
+
+	v.clear();
+	cout << v.size() << " " << v.capacity() << endl;
+
+
+	return 0;
+}
