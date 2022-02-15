45d16efb6f5817654872f36578e24b81fc64ab41
Joohyuck <joohyuck.seo@gmail.com>
2022-02-16 04:27:54 +0900

    오른손 법칙 개선

--- Maze/Player.cpp
+++ Maze/Player.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 #include "Player.h"
 #include "Board.h"
+#include <stack>
 
 void Player::Init(Board* board)
 {
@@ -51,6 +52,30 @@ void Player::Init(Board* board)
 			_dir = (_dir + 1) % DIR_COUNT;
 		}
 	}
+
+	stack<Pos> s;
+	for (int i = 0; i < _path.size() - 1; i++)
+	{
+		if (s.empty() == false && s.top() == _path[i + 1])
+			s.pop();
+		else
+			s.push(_path[i]);
+	}
+
+	// 목적지 도착
+	if (_path.empty() == false)
+		s.push(_path.back());
+
+	vector<Pos> path;
+	while (s.empty() == false)
+	{
+		path.push_back(s.top());
+		s.pop();
+	}
+
+	std::reverse(path.begin(), path.end());
+
+	_path = path;
 }
 
 void Player::Update(uint64 deltaTick)
