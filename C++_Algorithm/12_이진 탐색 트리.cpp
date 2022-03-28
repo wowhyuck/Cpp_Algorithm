b896ab50d3424763b9055da32c2992f9ca7e93d2
Joohyuck <joohyuck.seo@gmail.com>
2022-03-29 06:25:29 +0900

    이진 탐색 트리

---
+++ C++_Algorithm/12_이진 탐색 트리.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+#include "BinarySearchTree.h"
+
+using namespace std;
+
+// 오늘의 주제 : 이진 탐색 트리
+
+int main()
+{
+	BinarySearchTree bst;
+
+	bst.Insert(20);
+	bst.Insert(10);
+	bst.Insert(30);
+	bst.Insert(25);
+	bst.Insert(26);
+	bst.Insert(40);
+	bst.Insert(50);
+
+	bst.Delete(20);
+	bst.Delete(26);
+
+	bst.Print();
+
+	return 0;
+}
---
+++ C++_Algorithm/BinarySearchTree.cpp
@@ -0,0 +1,182 @@
+﻿#include "BinarySearchTree.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void SetCursorPosition(int x, int y)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { static_cast<SHORT>(x), static_cast<SHORT>(y) };
+	::SetConsoleCursorPosition(output, pos);
+}
+
+void BinarySearchTree::Print(Node* node, int x, int y)
+{
+	if (node == nullptr)
+		return;
+
+	SetCursorPosition(x, y);
+	cout << node->key;
+	Print(node->left, x - (5 / (y + 1)), y + 1);
+	Print(node->right, x + (5 / (y + 1)), y + 1);
+}
+
+void BinarySearchTree::Print_Inorder(Node* node)
+{
+	// 전위 순회 (preorder traverse)
+	// 중위 순회 (inorder)
+	// 후위 순회 (postorder)
+
+	if (node == nullptr)
+		return;
+
+	//    [중]
+	// [좌]  [우]
+
+	// 전위 : [중]이 앞에 온다. [중][좌][우]
+	cout << node->key << endl;
+	Print_Inorder(node->left);
+	Print_Inorder(node->right);
+
+	// 중위 : [중]이 중간에 온다. [좌][중][우]
+	//Print_Inorder(node->left);
+	//cout << node->key << endl;
+	//Print_Inorder(node->right);
+	
+	// 후위 : [중]이 마지막에 온다. [좌][우][중]
+	//Print_Inorder(node->left);
+	//Print_Inorder(node->right);
+	//cout << node->key << endl;
+
+}
+
+// 재귀함수 방법
+Node* BinarySearchTree::Search(Node* node, int key)
+{
+	if (node == nullptr || key == node->key)
+		return node;
+
+	if (key < node->key)
+		return Search(node->left, key);
+	else
+		return Search(node->right, key);
+}
+
+// while문 방법
+Node* BinarySearchTree::Search2(Node* node, int key)
+{
+	while (node && key != node->key)
+	{
+		if (key < node->key)
+			node = node->left;
+		else
+			node = node->right;
+	}
+
+	return node;
+}
+
+Node* BinarySearchTree::Min(Node* node)
+{
+	while (node->left)
+		node = node->left;
+
+	return node;
+}
+
+Node* BinarySearchTree::Max(Node* node)
+{
+	while (node->right)
+		node = node->right;
+
+	return node;
+}
+
+Node* BinarySearchTree::Next(Node* node)
+{
+	if (node->right)
+		return Min(node->right);
+
+	Node* parent = node->parent;
+
+	while (parent && node == parent->right)
+	{
+		node = parent;
+		parent = parent->parent;
+	}
+
+	return parent;
+}
+
+void BinarySearchTree::Insert(int key)
+{
+	Node* newNode = new Node();
+	newNode->key = key;
+
+	if (_root == nullptr)
+	{
+		_root = newNode;
+		return;
+	}
+
+	Node* node = _root;
+	Node* parent = nullptr;
+	
+	while (node)
+	{
+		parent = node;
+		if (key < node->key)
+			node = node->left;
+		else
+			node = node->right;
+	}
+
+	newNode->parent = parent;
+
+	if (key < parent->key)
+		parent->left = newNode;
+	else
+		parent->right = newNode;
+}
+
+void BinarySearchTree::Delete(int key)
+{
+	Node* deleteNode = Search(_root, key);
+	Delete(deleteNode);
+}
+
+void BinarySearchTree::Delete(Node* node)
+{
+	if (node == nullptr)
+		return;
+
+	if (node->left == nullptr)
+		Replace(node, node->right);
+	else if (node->right == nullptr)
+		Replace(node, node->left);
+	else
+	{
+		// 다음 데이터 찾기
+		Node* next = Next(node);
+		node->key = next->key;
+		Delete(next);
+	}
+}
+
+// u 서브트리를 v 서브트리로 교체
+// 그리고 delete u
+void BinarySearchTree::Replace(Node* u, Node* v)
+{
+	if (u->parent == nullptr)
+		_root = v;
+	else if (u == u->parent->left)
+		u->parent->left = v;
+	else
+		u->parent->right = v;
+
+	if (v)
+		v->parent = u->parent;
+
+	delete u;
+}
