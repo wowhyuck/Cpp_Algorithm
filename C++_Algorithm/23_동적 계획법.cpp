ef2bcf3e69551e429d5125eddf6b798ddf11f008
Joohyuck <joohyuck.seo@gmail.com>
2022-06-29 16:35:11 +0900

    동적 계획법

---
+++ C++_Algorithm/23_동적 계획법.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+// 오늘의 주제 : 동적 계획법 (DP)
+
+// 메모이제이션 (memoization)
+int cache[50][50];
+
+int combination(int n, int r)
+{
+	// 기저 사례
+	if (r == 0 || n == r)
+		return 1;
+
+	// 이미 답을 구한 적이 있으면 바로 반환
+	int& ret = cache[n][r];
+	if (ret != -1)
+		return ret;
+
+	// 새로 답을 구해서 캐시에 저장
+	ret = combination(n - 1, r - 1) + combination(n - 1, r);
+	return ret;
+}
+
+
+int main()
+{
+	::memset(cache, -1, sizeof(cache));
+
+	__int64 start = GetTickCount64();
+
+	int lotto = combination(45, 6);
+
+	__int64 end = GetTickCount64();
+
+	cout << end - start << "ms" << endl;
+
+	return 0;
+}
